eb1fc53800340fea5b62345a8b5b9f58c2d6c45e
csor <csor@inbox.ru>
2024-08-29 12:30:28 +0600

    Создал проект с приложениями api_v1, webapp

---
+++ api_v1/views.py
@@ -0,0 +1,47 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+import json
+
+
+@csrf_exempt
+@require_POST
+def add(request):
+    return calculate(request, lambda a, b: a + b)
+
+
+@csrf_exempt
+@require_POST
+def subtract(request):
+    return calculate(request, lambda a, b: a - b)
+
+
+@csrf_exempt
+@require_POST
+def multiply(request):
+    return calculate(request, lambda a, b: a * b)
+
+
+@csrf_exempt
+@require_POST
+def divide(request):
+    try:
+        return calculate(request, lambda a, b: a / b)
+    except ZeroDivisionError:
+        return JsonResponse({"error": "Division by zero!"}, status=400)
+
+
+def calculate(request, operation):
+    try:
+        data = json.loads(request.body)
+        a = data.get('A')
+        b = data.get('B')
+
+        if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+            raise ValueError("Inputs must be numbers")
+
+        result = operation(a, b)
+        return JsonResponse({"answer": result})
+
+    except (ValueError, KeyError) as e:
+        return JsonResponse({"error": str(e)}, status=400)
